c434cf66e793a0812dcf04a39fdb9645e8855f33
AbdAlrahim Zakaria <20120205314@ismu.edu.tr>
2023-12-01 11:34:57 +0300

    data validation and git ignore

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,85 +1,144 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = mongoose.Schema(
+  {
+    id: {
+      // Hide the id field (while keeping _id)
+      type: String,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      minlength: [10, 'Tour name must be longer or equal than 10 letters'],
+      maxlength: [50, 'Tour name must be shorter or equal than 50 letters'],
+      // validate: [validator.isAlpha, 'Tour name must consist of letters only'],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be either on of [easy, medium, difficutl]',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'AvgRating must be higher or equal than 1'],
+      max: [5, 'AvgRating must be lower or equal than 5'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have an image cover'],
+    },
+    images: [String],
+    startDates: [String],
+    rating: {
+      type: Number,
+      required: false,
+      default: 4.5,
+      min: [1, 'Rating must be higher or equal than 1'],
+      max: [5, 'Rating must be lower or equal than 5'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      default: 0,
+      validate: {
+        // "this" Only works on current document creation (doesn't work on updating)
+        validator: function (value) {
+          return this.price > value;
+        },
+        message: 'Price discount ({VALUE}) must be lower than the normal price',
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  priceDiscount: {
-    type: Number,
-    default: 0,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have an image cover'],
-  },
-  images: [String],
-  startDates: [String],
-  rating: {
-    type: Number,
-    required: false,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
-/**
- *   {
-    "id": 0,
-    "name": "The Forest Hiker",
-    "duration": 5,
-    "maxGroupSize": 25,
-    "difficulty": "easy",
-    "ratingsAverage": 4.7,
-    "ratingsQuantity": 37,
-    "price": 397,
-    "summary": "Breathtaking hike through the Canadian Banff National Park",
-    "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "imageCover": "tour-1-cover.jpg",
-    "images": ["tour-1-1.jpg", "tour-1-2.jpg", "tour-1-3.jpg"],
-    "startDates": ["2021-04-25,10:00", "2021-07-20,10:00", "2021-10-05,10:00"]
-  },
+// DOCUMENT MIDDLEWARE
+// Only works for .save() & .create(), used to modify the document before saving it
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
- */
+// tourSchema.pre('save', (next) => {
+//   console.log('Saving document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.queryTime = Date.now();
+  next();
+});
+
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(docs);
+//   console.log(`Query time is ${Date.now() - this.queryTime}`);
+//   next();
+// });
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  // console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
